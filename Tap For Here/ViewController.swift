4f097737cb3dd716c2041f4e8fc4d69b646f8256
Zachary Solomon <zacharysolomon@Zacharys-MacBook-Pro.local>
2019-03-15 10:48:25 -0400

    Added second view and added functions for UI elements

--- Tap For Here/ViewController.swift
+++ Tap For Here/ViewController.swift
@@ -19,28 +19,38 @@ class ViewController: UIViewController{
     
     
     
+    @IBOutlet weak var topButton: UIButton!
     @IBOutlet weak var roundedCornerButton: UIButton!
     var ref: DatabaseReference!
     var session: NFCNDEFReaderSession?
     
     let terst = NFCReadTag()
     
-
     override func viewDidLoad() {
         super.viewDidLoad()
+        topButton.layer.cornerRadius = 40
         roundedCornerButton.layer.cornerRadius = 26
-        // Do any additional setup after loading the view, typically from a nib.
         
     }
-
-
-    @IBAction func action(_ sender: Any) {
+    
+    //This function occurs when you tap for here
+    @IBAction func topButtonAction(_ sender: Any) {
         print("*****")
-    
+        
         terst.start() // Read NFC Tag
-        terst
+    }
+    
+    //This function occurs when you tap "Register"
+    @IBAction func action(_ sender: Any) {
+        
         
     }
     
-    
+    //This function occurs during the transition from ViewController to SecondViewController ***also when you tap "Register"****
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "showSecondViewController" {
+            let secondViewController = segue.destination as! SecondViewController
+            secondViewController.textField1.text = "Hello, World"
+        }
+    }
 }
